e5f6efa2e68e45465056b460de100b15eecfb0ef
crystalsz <70886193+crystalsz@users.noreply.github.com>
2021-04-14 16:44:21 +0900

    Add files via upload

--- ArrayReturn.java
+++ ArrayReturn.java
@@ -1,10 +1,25 @@
-package review;
 
 public class ArrayReturn {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int intArray[] = {3, 4, 5, 6, 7, 8}; int intArr2[] = new int[4];
+		int sum = 0;
+		sum = hap(intArray);
+		System.out.println("รัวี = " + sum);
+		intArr2 = hap2();
+		for(int i = 0; i < intArr2.length; i++)
+			System.out.printf("%3d", intArr2[i]);
 	}
-
+	static int hap(int [] abc) {
+		int hap = 0;
+		for(int i = 0; i < abc.length; i++)
+			hap += abc[i];
+		return hap;
+		
+	}
+	static int [] hap2() {
+		int hap[] = {3, 4, 5, 6};
+		for(int i = 0; i < hap.length; i++)
+			hap[i] = hap[i] + 5;
+		return hap;
+	}
 }
