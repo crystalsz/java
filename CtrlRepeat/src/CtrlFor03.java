d32f4f39186f747aab5012b30a63b8a625b58d82
crystalsz <70886193+crystalsz@users.noreply.github.com>
2021-06-04 10:19:06 +0900

    Add files via upload

---
+++ CtrlRepeat/src/CtrlFor03.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class CtrlFor03 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.print("Á¤¼ö : ");
+		n = sc.nextInt();
+		int i, sum=0;
+		for(i = 1; i<=10; i++) {
+			sum += i;
+			
+				System.out.print(i);
+				if(i <= n-1)
+				System.out.print("+");
+				else
+					System.out.print("=");
+		}
+		System.out.println(sum);
+		System.out.println(i);
+	}
+
+}
