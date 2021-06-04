8f6d4c7045e0b70519f0cb45f747ae107e5520bf
crystalsz <70886193+crystalsz@users.noreply.github.com>
2021-06-04 10:13:18 +0900

    Add files via upload

---
+++ staticClass/src/staticClass/Abc.java
@@ -0,0 +1,13 @@
+package staticClass;
+
+public class Abc {
+	int i;
+	public static int display() {
+		System.out.println("학번");
+		System.out.println("이름");
+		return 6;
+	}
+	public static void main(String[] args) {
+		display();
+	}
+}
